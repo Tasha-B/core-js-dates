2016810c5a2b9544bd5c034c7851f7f97ce10d8e
Natalia Belyatskaya <natbeliatsky@gmail.com>
2024-03-26 00:06:15 +0300

    feat: add all tasks solutions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -132,8 +132,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const day = new Date(date);
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+  return day >= start && day <= end;
 }
 
 /**
@@ -147,8 +150,8 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en-US', { timeZone: 'UTC' });
 }
 
 /**
@@ -206,8 +209,15 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const data = new Date(date);
+  if (data.getDate() < 13) {
+    data.setDate(13);
+  }
+  while (data.getDay() !== 5) {
+    data.setMonth(data.getMonth() + 1);
+  }
+  return data;
 }
 
 /**
@@ -243,8 +253,27 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const start = new Date(period.start.split('-').reverse().join('-'));
+  const end = new Date(period.end.split('-').reverse().join('-'));
+  const schedule = [];
+  const data = new Date(start);
+  let workDays = 0;
+
+  while (data <= end) {
+    if (workDays < countWorkDays) {
+      const day = data.getDate().toString().padStart(2, '0');
+      const month = (data.getMonth() + 1).toString().padStart(2, '0');
+      const year = data.getFullYear();
+      schedule.push(`${day}-${month}-${year}`);
+      workDays += 1;
+    } else {
+      workDays = 0;
+      data.setDate(data.getDate() + countOffDays - 1);
+    }
+    data.setDate(data.getDate() + 1);
+  }
+  return schedule;
 }
 
 /**
